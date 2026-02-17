90f84e80ee31303073bbbb0c52ef37b5c6caf777
Ana Elena Velasco García <143124370+Anaaveg17@users.noreply.github.com>
2026-02-17 08:18:19 -0600

    session-2026-02-17

---
+++ sql_challenges/challenge-02/solution.sql
@@ -0,0 +1,34 @@
+-- Ex 6
+
+-- 1 
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+
+-- 2
+SELECT title, domestic_sales, international_sales 
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+
+-- 3
+SELECT title, rating
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id
+ORDER BY rating DESC;
+ 
+
+-- Ex 7
+
+-- 1
+SELECT DISTINCT building FROM employees;
+
+-- 2
+SELECT * FROM buildings;
+
+-- 3
+SELECT DISTINCT building_name, role 
+FROM buildings 
+  LEFT JOIN employees
+    ON building_name = building;
